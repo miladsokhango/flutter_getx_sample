791d158fbaf5163e7877d7a6c9499d7046c0fe0b
miladsokhango <miladmilad7395@gmail.com>
2022-06-22 20:55:45 +0430

    Rx

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,12 +1,18 @@
 
 
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_sample/model/product_model.dart';
 import 'package:get/get.dart';
 
+import '../model/product_model.dart';
+
+// ignore: use_key_in_widget_constructors, must_be_immutable
 class MainScreen extends StatelessWidget{
 
-  var counteer = 0.obs;
+  // var counteer = 0.obs;
+
+  Rx<ProductModel> productModel = ProductModel(name: "پفک نمکی", price: "1000000", off: "50%").obs;
 
 
   @override
@@ -18,18 +24,28 @@ class MainScreen extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Obx(((){
-              return Text("Counter : $counteer", style: TextStyle(fontSize: 30),);
+              return Column(
+                children: [
+                  Text("product name : ${productModel.value.name}", style: const TextStyle(fontSize: 20),),
+                   Text("product price : ${productModel.value.price}", style: const TextStyle(fontSize: 20),),
+                    Text("product off : ${productModel.value.off}", style: const TextStyle(fontSize: 20),),
+                ],
+              );
             })
               
             ),
                
             ElevatedButton(
               onPressed: ((){
-                counteer=counteer+1;
+                productModel.update((val) {
+                  val!.name ="chitooz namaki";
+                  val!.price ="10000";
+                  val!.off ="50%";
+                });
               })
 
             ,
-            child: Text("Press"))
+            child: const Text("Press"))
           ],
         ),
       ),
